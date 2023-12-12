1b3aa0d15b0c663bef913ec38b6708135739351d
Rashid Ameer <r.ameer.offical@gmail.com>
2023-12-12 16:10:56 +0500

    Added Users Component and fetched users

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,7 @@
+type Props = {
+	params: { userId: string };
+};
+function User({ params: { userId } }: Props) {
+	return <div>User</div>;
+}
+export default User;
---
+++ app/users/page.tsx
@@ -0,0 +1,23 @@
+import getAllUsers from "@/lib/getAllUsers";
+import Link from "next/link";
+
+async function Users() {
+	const usersPromise: Promise<User[]> = getAllUsers();
+
+	const users = await usersPromise;
+
+	return (
+		<div className="px-4">
+			<h2 className="mt-2 mb-4 font-medium text-slate-700 text-2xl">Users</h2>
+
+			<div>
+				{users.map((user) => (
+					<div key={user.id} className="mb-2 last:mb-0 py-3 bg-blue-100 px-4 rounded-md">
+						<Link href={`/users/${1}`}>{user.name}</Link>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+export default Users;
